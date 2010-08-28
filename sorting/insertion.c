4d6d470cd2aa9f99dd8a34257f99efa138132ace
Igor Sobreira <igor@igorsobreira.com>
2010-08-28 02:03:52 -0300

    organizing insertion sorting implementation

--- sorting/insertion.c
+++ sorting/insertion.c
@@ -3,10 +3,7 @@
  */
 #include <stdio.h>
 
-int main() {
-    
-    int numbers[5] = {6, 2, 9, 4, 3};
-    int length = 5;
+void sort(int numbers[], int length) {
     int i, j, tmp;
 
     for (i = 1; i < length; i++) {
@@ -16,11 +13,26 @@ int main() {
         }
         numbers[j] = tmp;
     }
+}
 
-    for (i = 0; i < length; i++) {
-        printf("%d ", numbers[i]);
+void assert_array(int expected[], int received[], int length) {
+    int i;
+
+    for (i = 0; i < length && expected[i] != received[i]; i++) {
+        printf("ASSERT FAILED: sort() function is wrong\n");
+        break;
     }
-    printf("\n");
+}
 
+int main() {
+    
+    int numbers[5] = {6, 2, 9, 4, 3};
+    int expected[5] = {2, 3, 4, 6, 9};
+    int length = 5;
+    int i;
+
+    sort(numbers, length);
+    assert_array(expected, numbers, length);
+     
     return 0;
 }
